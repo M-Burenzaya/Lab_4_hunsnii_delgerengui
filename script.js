8c473ad3f3d82201db553b062b2d59efade47237
Burenzaya <bvrnee1009@gmail.com>
2023-10-22 21:38:29 +0800

    added hover effect and buttton click function

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('potato').addEventListener('click', function() {
+    // JavaScript code to run when cell 1 is clicked
+    alert('Cell 1 clicked!');
+});
+
+document.getElementById('carrot').addEventListener('click', function() {
+    // JavaScript code to run when cell 2 is clicked
+    alert('Cell 2 clicked!');
+});
+
+document.getElementById('cabbage').addEventListener('click', function() {
+    // JavaScript code to run when cell 2 is clicked
+    alert('Cell 3 clicked!');
+});
